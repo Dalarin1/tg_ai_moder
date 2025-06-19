2a0249d36eae10c692a2c08e2fb53dc07094771d
Dalarin1 <103304145+Dalarin1@users.noreply.github.com>
2025-06-19 18:39:20 +0500

    Delete tox_analiser.py

--- tox_analiser.py
+++
@@ -1,7 +0,0 @@
-from transformers import BertTokenizer, BertForSequenceClassification
-
-tokenizer = BertTokenizer.from_pretrained('bert-tox-detecter')
-model = BertForSequenceClassification.from_pretrained('bert-tox-detecter')
-
-def is_toxic(text:str):
-    return model(tokenizer.encode(text,return_tensors='pt')).logits.tolist()[0][1]  > 1
